51293593fe99ad825316540922d54e6069840ae1
Maria Luisa <marialuisaufmg@gmail.com>
2019-04-01 00:46:14 -0300

    palavras

--- text_mining.py
+++ text_mining.py
@@ -259,7 +259,7 @@ class TextProcessing():
         #     test_set =  mac_morpho.tagged_sents()[10001:10010]
 
         #     print 'antes do mac morpho'
-            unigram_tagger = nltk.UnigramTagger(train_set)
+            tagger = nltk.UnigramTagger(train_set)
             # tagger = nltk.BigramTagger(train_set)
         #     print 'depois do mac morpho'
 
